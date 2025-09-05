03ba9460fd56f6755f5dd6a9bdc33be887ee7b9d
AtwijukaAbcj <charles.bravo24@gmiail.com>
2025-09-05 23:35:35 +0300

    Add collateral security support: migration, model, Filament resource

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Models\Role;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use App\Filament\Resources\RoleResource\Pages;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+    protected static ?string $navigationGroup = 'User Management';
+    protected static ?string $navigationIcon = 'heroicon-o-shield-check';
+    protected static ?string $navigationLabel = 'Roles';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('name')->required()->maxLength(255),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            Tables\Columns\TextColumn::make('name')->searchable(),
+        ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
--- resources/views/deletes/borrower.blade.php
+++ resources/views/deletes/borrower.blade.php
@@ -1,5 +1,47 @@
-<form action="{{ route('borrower.destroy', $borrower->id) }}" method="POST" style="display: inline;">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-danger">Delete Borrower</button>
-</form>
+<div class="container">
+    <h1>View Borrower</h1>
+    <form>
+        <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 2rem;">
+            <div>
+                <label>First Name</label>
+                <input type="text" class="form-control" value="{{ $borrower->first_name }}" disabled>
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input type="text" class="form-control" value="{{ $borrower->last_name }}" disabled>
+            </div>
+            <div>
+                <label>Full Name</label>
+                <input type="text" class="form-control" value="{{ $borrower->full_name }}" disabled>
+            </div>
+            <div>
+                <label>Gender</label>
+                <input type="text" class="form-control" value="{{ $borrower->gender }}" disabled>
+            </div>
+            <div>
+                <label>Date of Birth</label>
+                <input type="text" class="form-control" value="{{ $borrower->date_of_birth }}" disabled>
+            </div>
+            <div>
+                <label>Occupation</label>
+                <input type="text" class="form-control" value="{{ $borrower->occupation }}" disabled>
+            </div>
+            <div>
+                <label>National ID</label>
+                <input type="text" class="form-control" value="{{ $borrower->national_id }}" disabled>
+            </div>
+            <div>
+                <label>Phone</label>
+                <input type="text" class="form-control" value="{{ $borrower->phone }}" disabled>
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="text" class="form-control" value="{{ $borrower->email }}" disabled>
+            </div>
+            <div style="grid-column: 1 / span 2;">
+                <label>Address</label>
+                <input type="text" class="form-control" value="{{ $borrower->address }}" disabled>
+            </div>
+        </div>
+    </form>
+</div>
