b31a990c906c0eb7e8869c99dcd466395920c92a
AtwijukaAbcj <charles.bravo24@gmiail.com>
2025-09-05 06:26:28 +0300

    Implement Pesapal payments for repayments and disbursements, add borrower UI

--- app/Filament/Pages/PesapalSettings.php
+++ app/Filament/Pages/PesapalSettings.php
@@ -1,15 +1,1 @@
-<?php
 
-namespace App\Filament\Pages;
-
-use Filament\Pages\Page;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Request;
-
-class PesapalSettings extends Page
-{
-    protected static ?string $navigationIcon = 'heroicon-o-cog';
-    protected static string $view = 'filament.pages.pesapal-settings';
-    // File removed: This Filament Page is obsolete and replaced by the Resource.
-}
