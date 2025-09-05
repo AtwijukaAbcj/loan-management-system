ae74042693c3bff2ef28079cebeab6aeb399fe03
AtwijukaAbcj <charles.bravo24@gmiail.com>
2025-09-05 22:40:27 +0300

    Add collateral security support: migration, model, Filament resource

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,23 +34,7 @@ class AdminPanelProvider extends PanelProvider
         ->id('admin')
         ->path('admin')
         ->plugins([
-            FilamentShieldPlugin::make()
-                ->gridColumns([
-                    'default' => 1,
-                    'sm' => 2,
-                    'lg' => 2
-                ])
-                ->sectionColumnSpan(1)
-                ->checkboxListColumns([
-                    'default' => 1,
-                    'sm' => 2,
-                    'lg' => 4,
-                ])
-                ->resourceCheckboxListColumns([
-                    'default' => 1,
-                    'sm' => 2,
-                ]),
-                ActivitylogPlugin::make()
+            ActivitylogPlugin::make()
                 ->authorize(
                     fn () => auth()->user()->hasRole('super_admin')
                 ),
