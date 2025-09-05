b1bf36ad117f3b8914ef0e467bb2aa724b66daa4
AtwijukaAbcj <charles.bravo24@gmiail.com>
2025-09-05 06:22:17 +0300

    Implement Pesapal payments for repayments and disbursements, add borrower UI

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -77,7 +77,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
-                \App\Filament\Pages\PesapalSettings::class,
+                    // ...existing code...
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
