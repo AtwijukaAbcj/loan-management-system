70fc68e52117b4cc2b61012ff1262550fe24d7a6
AtwijukaAbcj <charles.bravo24@gmiail.com>
2025-09-05 06:24:40 +0300

    Implement Pesapal payments for repayments and disbursements, add borrower UI

--- app/Filament/Pages/PesapalSettings.php
+++ app/Filament/Pages/PesapalSettings.php
@@ -11,34 +11,5 @@ class PesapalSettings extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-cog';
     protected static string $view = 'filament.pages.pesapal-settings';
-    protected static ?string $navigationLabel = 'Pesapal Settings';
-    protected static ?string $title = 'Pesapal API Settings';
-    protected static ?string $navigationGroup = 'Addons';
-
-    public $consumer_key;
-    public $consumer_secret;
-    public $test_result;
-    public function testKeys()
-    {
-        // Simulate a test call to Pesapal API
-        if ($this->consumer_key && $this->consumer_secret) {
-            $this->test_result = 'Test successful! Keys are set.';
-        } else {
-            $this->test_result = 'Test failed! Please enter both keys.';
-        }
-    }
-
-    public function mount()
-    {
-        $this->consumer_key = env('PESAPAL_CONSUMER_KEY');
-        $this->consumer_secret = env('PESAPAL_CONSUMER_SECRET');
-    }
-
-    public function save()
-    {
-        // For demo: just flash values. In production, save to DB or .env
-        Session::flash('success', 'Pesapal keys saved!');
-        Session::flash('consumer_key', $this->consumer_key);
-        Session::flash('consumer_secret', $this->consumer_secret);
-    }
+    // File removed: This Filament Page is obsolete and replaced by the Resource.
 }
