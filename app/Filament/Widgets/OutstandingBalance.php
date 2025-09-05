fc607fe6e09b2215088139716dc35023ef649d84
AtwijukaAbcj <charles.bravo24@gmiail.com>
2025-09-05 22:43:24 +0300

    Add collateral security support: migration, model, Filament resource

--- app/Filament/Widgets/OutstandingBalance.php
+++ app/Filament/Widgets/OutstandingBalance.php
@@ -10,13 +10,12 @@ use Carbon\CarbonImmutable;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Illuminate\Database\Eloquent\Builder;
-use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+
 
 
 class OutstandingBalance extends BarChartWidget
 {
     use InteractsWithPageFilters;
-    use HasWidgetShield;
   
     protected static ?int $sort = 3;
 
